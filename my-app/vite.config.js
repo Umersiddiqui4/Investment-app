f637212e79bb5ecda9d6630443056d96abc6ded6
Umer-Siddiqui <umermun12@gmail.com>
2025-04-13 15:20:21 +0500

    Feature: vercel.json file added

--- my-app/vite.config.js
+++ my-app/vite.config.js
@@ -3,11 +3,22 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
-  plugins: [react(),
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
+  server: {
+    // Local dev server ke liye fallback (optional)
+    historyApiFallback: true,
+  },
+  build: {
+    // Ye production build ke liye hota hai
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+      },
+    },
+  }
 })
